649a51ebe99cd5cb162d1a6752305d37221792f3
darkLord19 <umangjparmar@gmail.com>
2024-05-06 22:09:50 +0530

    customize plugin starter template

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/mattermost/mattermost-plugin-starter-template
+module github.com/darkLord19/mattermost-plugin-google-drive
 
 go 1.21
 
--- webapp/src/manifest.test.tsx
+++
@@ -1,7 +0,0 @@
-import manifest from './manifest';
-
-test('Plugin manifest, id and version are defined', () => {
-    expect(manifest).toBeDefined();
-    expect(manifest.id).toBeDefined();
-    expect(manifest.version).toBeDefined();
-});
