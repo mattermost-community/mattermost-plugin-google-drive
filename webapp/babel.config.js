e2407d62bee04b3b98c8cb688103066e86bd0235
kshitij katiyar <kshitij.katiyar@brightscout.com>
2025-05-08 14:04:22 +0530

    Upgrade node version to 20.11

--- webapp/babel.config.js
+++ webapp/babel.config.js
@@ -28,7 +28,7 @@ const config = {
     plugins: [
         '@babel/plugin-proposal-class-properties',
         '@babel/plugin-syntax-dynamic-import',
-        '@babel/proposal-object-rest-spread',
+        '@babel/plugin-proposal-object-rest-spread',
         '@babel/plugin-proposal-optional-chaining',
         'babel-plugin-typescript-to-proptypes',
     ],
